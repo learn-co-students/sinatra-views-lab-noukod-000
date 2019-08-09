ae6112b1a86ae95aaf39200ad8088b42749187c8
Rachele Suy <rachelesuy@gmail.com>
2019-08-09 03:43:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
-
-
+	get '/date' do
+		erb :date	
+	  end
+    get '/goodbye' do
+		#"Hello World"
+		erb :goodbye
+	  end
+	  get '/hello' do
+		#"Hello World"
+		erb :hello
+	  end
 end
